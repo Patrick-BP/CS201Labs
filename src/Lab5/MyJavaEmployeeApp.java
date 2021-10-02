e1f0c85ae1bc64e8f873395e2c2890aa1611132c
Patrick <papybp@gmail.com>
2021-10-01 22:20:46 -0500

    lab5 update 1 oct

--- src/Lab5/MyJavaEmployeeApp.java
+++ src/Lab5/MyJavaEmployeeApp.java
@@ -2,7 +2,7 @@ package Lab5;
 
 public class MyJavaEmployeeApp {
     public static void main(String[] args) {
-        MyJavaEmployee emp1 = new MyJavaEmployee(9.0,300);
+        MyJavaEmployee emp1 = new MyJavaEmployee(15,45);
         emp1.wage();
 
     }
