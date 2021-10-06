74070ad29b48d44a4d5f8fc2983993db4ac7e746
Patrick <papybp@gmail.com>
2021-10-06 18:14:45 -0500

    lab8and9 6oct

--- src/Lab6/Question3.java
+++ src/Lab6/Question3.java
@@ -1,34 +1,35 @@
-//package Lab6;
-//
-//public class Question3 {
-//
-//    int x = 10,
-//    y=20,
-//    z=30;
-//
-//    //a.
-//    x < 10 || x > 10; // False
-//
-//    //b.
-//    x > y && y > x // always False
-//
-//    //c.
-//    (x < y + z) && (x + 10 <=20) // True
-//
-//    //d.
-//    z - y == x && Math.abs(x - z) == x // True
-//
-//    //e.
-//    x < 10 && x > 10 // always False
-//
-//    //f.
-//    x > y || y > x // True
-//
-//    //g.
-//    !(x<y+z)||!(x+10<=20) // False
-//
-//     //h.
-//    !(x==y) &&(x !=y) && (x<y || y<x) // True
-//
-//
-//}
+package Lab6;
+
+public class Question3 {
+
+    int x = 10,
+    y=20,
+    z=30;
+
+  /*
+   a.
+    x < 10 || x > 10;    ==> False
+
+    b.
+    x > y && y > x        ==> always False
+
+    c.
+    (x < y + z) && (x + 10 <=20)    ==> True
+
+    d.
+    z - y == x && Math.abs(x - z) == x     ==> True
+
+    e.
+    x < 10 && x > 10     ==> always False
+
+    f.
+    x > y || y > x     ==> True
+
+    g.
+    !(x<y+z)||!(x+10<=20)     ==> False
+
+     h.
+    !(x==y) &&(x !=y) && (x<y || y<x)   ==> True
+*/
+
+}
