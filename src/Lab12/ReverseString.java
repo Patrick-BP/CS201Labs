3ffca0e5e367cb14b9ad95731bc70b36226b28ee
Patrick <papybp@gmail.com>
2021-10-13 23:57:01 -0500

    lab5 update 1 oct

--- src/Lab12/ReverseString.java
+++ src/Lab12/ReverseString.java
@@ -1,15 +1,15 @@
-//package Lab12;
-//
-//public class ReverseString {
-//    public static void main(String[] args) {
-//        String str = "patrick";
-//        int index = str.length();
-//        System.out.println(reverseString(str,index));
-//    }
-//    public static String reverseString(String str , int index){
-//        if(index)
-//            return
-//
-//
-//    }
-//}
+package Lab12;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "patrick";
+        int index = str.length();
+        System.out.println(reverseString(str));
+    }
+    public static String reverseString(String str){
+        if (str.isEmpty()) return str;
+        else
+          return  reverseString(str.substring(1))+str.charAt(0);
+
+    }
+}
